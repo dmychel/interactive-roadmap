ade3980f0196aec38d4088fb8f20400adeb3d50e
Dylon <dmychel@tutanota.com>
2024-12-10 00:27:57 -0500

    created dynamic grid logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
 import "/src/App.css";
 
 function App() {
+  const arr = [
+    "Newsletter",
+    1,
+    2,
+    3,
+    "SEO",
+    1,
+    2,
+    3,
+    "Backups",
+    1,
+    2,
+    3,
+    "Premium",
+    1,
+    2,
+    3,
+  ];
   return (
     <div className="app">
       <h1>hello world</h1>
+      <div className="grid">
+        {/* Header Progress */}
+        <span> </span>
+        <div className="table_heading">Backlog</div>
+        <div className="table_heading">In Developevment</div>
+        <div className="table_heading">Completed</div>
+
+        {arr.map((tile, index) => {
+          // console.log(tile);
+          // console.log(index);
+          if (index === 0) {
+            return (
+              <div className="tile" key={index}>
+                {tile}
+              </div>
+            );
+          } else if (index % 4 === 0) {
+            console.log(index);
+            return (
+              <div className="tile" key={index}>
+                {tile}
+              </div>
+            );
+          } else {
+            return <div key={index}></div>;
+          }
+        })}
+      </div>
     </div>
   );
 }
